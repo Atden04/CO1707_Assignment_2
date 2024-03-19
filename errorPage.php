6d7e0713e5ce65fc557f5bafd16abaed28df7923
Alexander Denton <atdenton2004@gmail.com>
2024-03-19 11:08:49 +0000

    error page does in fact work. just need footer to be at the bottom.

---
+++ errorPage.php
@@ -0,0 +1,102 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- 
+Author: Alexander Denton
+ID : 21002180
+Date: 14/01/2024
+Assignment 1 index page
+-->
+
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css"> <!-- link to the stylesheet -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Student Shop</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <header>
+        <!-- header bar that is going to appear at the top of the screen  -->
+        <div class="logo"><img src="images/logo.svg" alt="UCLan logo"></div>
+        <div class="siteName">Student Shop</div>
+        <!-- place the navigation links in a <nav> tag for accessibility purposes -->
+        <nav id="desktopNav"> <!-- place the navigation links in a <nav> tag for accessibility purposes -->
+            <ul id="desktopNavList">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="products.php">Products</a></li>
+                <li><a href="cart.php">Cart</a></li>
+                <?php
+                    if (isset($_SESSION["loggedIn"]) && $_SESSION["loggedIn"]) {
+                        echo "<li><a href='logout.php?returnPage=index.php'>Log Out</a></li>";
+                    } else {
+                        echo "<li><a href='signup.php'>Sign Up</a></li>";
+                    }
+                ?>
+            </ul>
+        </nav>
+        <!-- for mobile navigation https://www.w3schools.com/howto/howto_js_mobile_navbar.asp -->
+
+        <!-- First div is for burger menu icon-->
+        <nav id="mobileNav">
+            <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
+            <a href="javascript:void(0);" class="icon" onclick="revealMobileNav()">
+                <i class="fa fa-bars"></i>
+            </a>
+            <!-- Navigation links (hidden by default) -->
+            <ul id="mobileNavList">
+                <il><a href="index.php">Home</a></il>
+                <il><a href="products.php">Products</a></il>
+                <il><a href="cart.php">Cart</a></il>
+                <?php
+                    if (isset($_SESSION["loggedIn"]) && $_SESSION["loggedIn"]) {
+                        echo "<li><a href='logout.php?returnPage=index.php'>Log Out</a></li>";
+                    } else {
+                        echo "<li><a href='signup.php'>Sign Up</a></li>";
+                    }
+                ?>
+            </ul>
+        </nav>
+    </header>
+    <!-- Content captured from Desing Requirements video -->
+    <main>
+        <p>Error page</p>
+    </main>
+
+    <!-- Footer -->
+    <footer>
+        <section class="footerSection">
+            <h3>Links</h3>
+            <!-- link to student's union website-->
+            <a href="https://www.uclansu.co.uk/">Students' Union</a>
+        </section>
+        <section class="footerSection">
+            <h3>Contact</h3>
+            <p>Email : suinformation@uclan.ac.uk</p>
+            <p>Phone: 01772 89 3000</p>
+        </section>
+        <section class="footerSection">
+            <h3>Location</h3>
+            <p>University of Central Lancashire Students' Union,<br>Fylde Road, Preston. PR1 7BY<br>Registered in
+                England<br>Company Number: 07623917<br>Registered Charity Number: 1142616</p>
+        </section>
+    </footer>
+    <script>
+        /* for showing the mobile navigation when using the hamburger icon*/
+        function revealMobileNav() {
+            var x = document.getElementById("mobileNavList");
+            if (x.style.display === "block") {
+                x.style.display = "none";
+            } else {
+                x.style.display = "block";
+            }
+        }
+    </script>
+</body>
+
+</html>
