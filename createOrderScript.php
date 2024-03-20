7352bbe8f550329cfaf40ab5eae38f24e262ac61
Alexander Denton <atdenton2004@gmail.com>
2024-03-20 11:53:50 +0000

    Removal of excess code.

--- createOrderScript.php
+++ createOrderScript.php
@@ -17,28 +17,6 @@ if (!empty($_COOKIE[$cookie_name]) && $_SESSION["loggedIn"]) {
     mysqli_query($connection, "INSERT INTO `tbl_orders`(`order_id`, `order_date`, `user_id`, `product_ids`) VALUES ('$id',NOW(),'$userID','$productIDs')");
 
     setcookie($cookie_name, "", time() - 3600); //then delete cookie containg order
-
-
-
-
-
-    /*$rowsExistingEmail = mysqli_query($connection, "SELECT * FROM `tbl_users` WHERE user_email LIKE '$email'");
-                    $countRows = mysqli_num_rows($rowsExistingEmail);
-                    if ($countRows == 0) {   //if no rows then account with provided email doesn't exist
-                        //get num rows for next id and add data to databse.
-                        $rowsInTable = mysqli_query($connection, "SELECT * FROM tbl_users;");
-                        $id = mysqli_num_rows($rowsInTable)+1;
-                        
-                        echo "<script>alert('You\`re account has now been registered. You can now log in via the cart. ');</script>";
-                    } else {
-                        echo "<script>alert('An account already exists with the email provided. Please log in via the cart.');</script>";
-                    }
-                    
-
-                    
-                }*/
-
-
 }
 
 echo "<script>alert('Your order has successfully been created.');";
