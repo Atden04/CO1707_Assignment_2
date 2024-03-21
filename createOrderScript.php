72a30f93c7cca8522f07945287f52b69a0c4c2ed
Alexander Denton <atdenton2004@gmail.com>
2024-03-21 19:04:52 +0000

    No need for timestamp or id when adding new rows to database.

--- createOrderScript.php
+++ createOrderScript.php
@@ -8,13 +8,11 @@ $cart = unserialize($_COOKIE[$cookie_name]);
 if (!empty($_COOKIE[$cookie_name]) && $_SESSION["loggedIn"]) {
     //setcookie($cookie_name, "", time() - 3600); 
 
-    $rowsInTable = mysqli_query($connection, "SELECT * FROM tbl_orders;");
-    $id = mysqli_num_rows($rowsInTable)+1;
     $userID = $_SESSION["userID"];
     $productIDs = unserialize($_COOKIE[$cookie_name]);;
     $productIDs = implode(",",$productIDs); // Use of implode function to convert from array to string
 
-    mysqli_query($connection, "INSERT INTO `tbl_orders`(`order_id`, `order_date`, `user_id`, `product_ids`) VALUES ('$id',NOW(),'$userID','$productIDs')");
+    mysqli_query($connection, "INSERT INTO `tbl_orders`(`user_id`, `product_ids`) VALUES ('$userID','$productIDs')");
 
     setcookie($cookie_name, "", time() - 3600); //then delete cookie containg order
 }
